996e09150029988b8325aaba785b9a4117678e7b
beamimc <beatrizcampillo29@gmail.com>
2025-09-15 14:34:41 -0400

    include type option in calc_skipped_exons

--- R/calc_logic.R
+++ R/calc_logic.R
@@ -1,13 +1,15 @@
 #' Calculate skipped exons from a GRanges object
 #' @param gr A GRanges object with exon annotations, including 'tx_id', 'exon',
 #' and 'coef_col' metadata columns.
-#' The 'coef_col' column should contain 1 for upregulated exons and -1 for downregulated exons.
+#' @param coef_col The name of the metadata column indicating upregulated (+1) and downregulated (-1) exons.
+#' @param type The type of overlap to consider when identifying skipped exons.
 #' @return A GRanges object with an additional 'event' metadata column indicating skipped exons.
 #' @import GenomicRanges
 #' @importFrom dplyr group_by mutate ungroup filter
 #' @importFrom plyranges filter_by_non_overlaps_directed
 #' @export          
-calc_skipped_exons <- function(gr, coef_col) {
+calc_skipped_exons <- function(gr, coef_col, type = c("in","over","boundary")) {
+  type <- match.arg(type) 
   # if preprocessing didn't happen
   if (!all(c("key","nexons","internal","event") %in% names(mcols(gr)))) {
     gr <- preprocess_input(gr, coef_col)
@@ -37,14 +39,33 @@ calc_skipped_exons <- function(gr, coef_col) {
   left_exons <- gr |> plyranges::slice(match(left_keys, key))
   right_exons <- gr |> plyranges::slice(match(right_keys, key))
 
-  # determine if the left and right exons of candidates
-  # were somewhere in the positive exon set
-  candidates <- candidates |>
-    plyranges::filter(
-        left_exons %in% pos_exons &
-        right_exons %in% pos_exons
-    )
-
+  candidates <- switch(
+    type,
+    "in" = {
+      # determine if the left and right exons of candidates are in the positive set
+      candidates <- candidates |>
+        plyranges::filter(
+            left_exons %in% pos_exons &
+            right_exons %in% pos_exons
+        )
+    },
+    "over" = {
+      candidates <- candidates |>
+        plyranges::filter(
+            left_exons %over% pos_exons &
+            right_exons %over% pos_exons
+        )
+    },
+    "boundary" = {
+      has_end_in   <- function(x, y) end(x)   %in% end(y)
+      has_start_in <- function(x, y) start(x) %in% start(y)
+      candidates <- candidates |>
+        plyranges::filter(
+          (left_exons  |> has_end_in(pos_exons)) &
+          (right_exons |> has_start_in(pos_exons))
+        )
+    }
+  )
   gr |>
     plyranges::mutate(
       event = ifelse(key %in% candidates$key, "skipped_exon", event)
