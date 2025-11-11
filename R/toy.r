66882bc00fcadf7c2e8c4fd86edfc43f2b616ed9
beamimc <beatrizcampillo29@gmail.com>
2025-11-11 14:17:37 -0500

    upate pltRanges multiple genes

--- R/toy.r
+++ R/toy.r
@@ -2,6 +2,7 @@ library(GenomicRanges)
 library(IRanges)
 library(wiggleplotr)
 library(plyranges)
+library(patchwork)
 
 gr <- GRanges(
   seqnames = "chr1",
@@ -27,14 +28,32 @@ pltRanges <- function(gr) {
     mcols(gr)$tx_id <- 1L
   }
 
-  # Split 
-  exons <- split(gr, list(gr$tx_id, !is.na(GenomicRanges::mcols(gr)$event)), drop = TRUE)
-   
-  p <- wiggleplotr::plotTranscripts(
-    exons = exons,
-    rescale_introns = TRUE
-  )
-  return(p)
+  #make a plot per gene_id
+  plots <- list()
+  n_genes <- length(unique(gr$gene_id))
+
+  for (gene in unique(gr$gene_id)) {
+    gr_gene <- gr[gr$gene_id == gene]
+    if (length(gr_gene) == 0) next
+    
+    exons <- split(
+      gr_gene,
+      list(gr_gene$tx_id, !is.na(GenomicRanges::mcols(gr_gene)$event)),
+      drop = TRUE
+    )
+    p <- wiggleplotr::plotTranscripts(
+      exons = exons,
+      rescale_introns = TRUE
+    ) +
+      ggplot2::ggtitle(gene)
+    
+    plots[[gene]] <- p
+  }
+
+  # Combine all plots into a grid (adjust ncol for layout)
+  combined_plot <- wrap_plots(plots, ncol = round(n_genes/2 + 1))  
+  combined_plot
+
 }
   
  
