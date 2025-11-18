78924b43946c042a409983462d28974f478ddfe7
beamimc <beatrizcampillo29@gmail.com>
2025-11-18 17:41:15 -0500

    add calc_a3ss_a5ss func

--- R/calc_logic.R
+++ R/calc_logic.R
@@ -142,9 +142,9 @@ calc_mutually_exclusive <- function(gr, coef_col, type = c("in", "over", "bounda
 #' Function to calculate retained introns given a GRanges object
 #' @param gr A GRanges object with metadata columns: 'exon_rank', 'gene_id', 'tx_id', and 'coef'.
 #' @return A GRanges object with an additional 'event' metadata column indicating retained introns.
-#' @import GenomicRanges
 #' @export      
-calc_retained_introns <- function(gr) {
+calc_retained_introns <- function(gr, coef_col){
+   # TO DO : fix coef_col argument and add to preprocess_input and then rename coef_col to coefs
     # find introns in the negative coef transcripts
     introns <- gr |> plyranges::filter(coefs < 0) |> find_introns()
 
@@ -181,3 +181,76 @@ calc_retained_introns <- function(gr) {
     }
   hits
   }
+
+
+#' Function to calcualte 5' and 3' alternative splice sites given a GRanges object
+#' @param gr A GRanges object with metadata columns: 'exon_rank', 'gene_id', 'tx_id', and 'coef'.
+#' @return A GRanges object with an additional 'event' metadata column indicating retained introns.
+#' @export      
+
+calc_a3ss_a5ss <- function(gr, coef_col ){
+  # if preprocessing didn't happen
+  if (!all(c("key", "nexons", "internal", "event") %in% names(mcols(gr)))) {
+    gr <- preprocess_input(gr, coef_col)
+  }
+  
+  # separate positive and negative exons
+  var <- rlang::sym(coef_col)
+  pos_exons <- gr |> plyranges::filter(sign(!!var) == 1)
+  neg_exons <- gr |> plyranges::filter(sign(!!var) == -1)
+
+  # candidates are pos exons that do not exacly match neg_exons (%in%)
+  # filter candidates pos_exons that are exactly the same as any neg_exons
+  # and keep only those that overlap any neg_exons
+  candidates <- pos_exons |> 
+                plyranges::filter(!(pos_exons %in% neg_exons)) |>
+                plyranges::filter_by_overlaps_directed(neg_exons)
+
+  # TO DO maybe: filter using ids in case the same range is pressent in different genes ???
+
+  #   pos_id <- paste0(seqnames(pos_exons), ":", start(pos_exons), "-", end(pos_exons), ":",
+  #                  strand(pos_exons), ":", pos_exons$gene_id, ":", pos_exons$tx_id)
+
+  # neg_id <- paste0(seqnames(neg_exons), ":", start(neg_exons), "-", end(neg_exons), ":",
+  #                  strand(neg_exons), ":", neg_exons$gene_id, ":", neg_exons$tx_id)
+
+  # candidates <- pos_exons[!(pos_id %in% neg_id)] |>
+  #   plyranges::filter_by_overlaps_directed(neg_exons)
+
+
+  hits <- GRanges()
+  if (length(candidates) == 0L) {
+    return(hits) #return unchanged if no candidates
+  }
+
+ for (i in seq_along(candidates)) {
+    cand <- candidates[i]  # a length-1 GRanges
+    # restric to check on the same gene in neg_exons
+    cand_neg_exons <- neg_exons |> 
+                  plyranges::filter(gene_id == cand$gene_id) 
+                  # plyranges::filter(!(. %in%c cand))
+                  # plyranges::filter_by_overlaps(cand)
+
+
+      matches <- cand_neg_exons %>%
+        plyranges::mutate(
+          match_start  = GenomicRanges::start(.)   %in% GenomicRanges::start(cand),
+          match_end = GenomicRanges::end(.) %in% GenomicRanges::end(cand)
+        ) |>
+         # only keep those that match on one end but not the other
+        plyranges::filter(match_start != match_end)
+        
+      if (length(matches) > 0L) {
+        event_type <- ifelse(matches$match_start, "a5ss", "a3ss")
+        txs <- unique(matches$tx_id)
+        cand_rep <- rep(cand, length(txs)) |>
+          plyranges::mutate(
+            event    = event_type,
+            tx_event = txs
+          )
+        hits <- c(hits, cand_rep)
+        }
+  }
+
+  hits
+}
