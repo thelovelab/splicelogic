d0a43cd059ab904a66301f8d0f45252f65a99707
beamimc <beatrizcampillo29@gmail.com>
2026-03-12 23:07:18 -0400

    clean import

--- R/mock_data.R
+++ R/mock_data.R
@@ -47,9 +47,7 @@ return(gr)
 #' This dataset is designed to include a mutually exclusive exons between exon_rank 3 of tx_id 1
 #' and exon_rank 3 of tx_id 2. There is also a skipped exon event at exon_rank 5 of tx_id 1.
 #' @return A GRanges object with two transcripts per gene and candidate logic
-#' @import GenomicRanges
-#' @return A GRanges object with two transcripts per gene
-#' @export 
+#' @export
 mx_mock_data <- function() {
   df1 <- data.frame(
     seqnames = "chr1",
@@ -80,9 +78,7 @@ return(gr)
 #' Create a sample GRanges with one negative coef transcript and one positive coef transcripts
 #' This dataset is designed to include no splicing events
 #' @return A GRanges object with two transcripts per gene and no candidate logic
-#' @import GenomicRanges
-#' @return A GRanges object with two transcripts per gene
-#' @export 
+#' @export
 no_event_mock_data <- function() {
   df1 <- data.frame(
     seqnames = "chr1",
