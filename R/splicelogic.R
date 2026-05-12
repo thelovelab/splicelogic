154d31731ae80772924170bee3d89d6cb7728f58
Beatriz Campillo <beatrizcampillo29@gmail.com>
2026-05-12 14:41:21 -0400

    typo

--- R/splicelogic.R
+++ R/splicelogic.R
@@ -1,5 +1,5 @@
 #' splicelogic: differential transcripts to splice events
 #' 
-#' For more details on the features of plryanges, read the vignette:
+#' For more details on the features of splicelogic, read the vignette:
 #' `browseVignettes(package = "splicelogic")`
 "_PACKAGE"
